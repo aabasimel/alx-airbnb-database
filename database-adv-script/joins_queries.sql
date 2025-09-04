8ec7a838897cf342d286d0202778cf16adc02377
Ahmed <abasimelahmed@gmail.com>
2025-09-04 20:43:32 +0200

    Added joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,20 @@
+-- Retrieve all bookings and their respective users
+SELECT 
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    b.created_at AS booking_created_at,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    u.phone_number,
+    u.role,
+    u.created_at AS user_created_at
+FROM Booking b
+INNER JOIN User u
+    ON b.user_id = u.user_id
+ORDER BY b.created_at DESC;
